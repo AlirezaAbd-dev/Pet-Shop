18edbf21178e13de847afa5429c7ce8d75f53616
0AliReza0 <alireza.abedi9310@gmail.com>
2024-06-09 20:10:23 +0330

    tips section is responsive

---
+++ src/routes/home/components/cards/tips-card.component.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import React from 'react';
+
+import Icon from '@/components/icon';
+
+import SvgCalendarDates from '@icons/calendar-dates.svg';
+
+const TipsCard = () => {
+  return (
+    <div className="flex flex-col items-start rounded-2xl md:rounded-3xl border border-nature-800 p-4">
+      <Image
+        src="/static/tip-cat.png"
+        alt="tip-cat"
+        width={288}
+        height={180}
+        className="w-full md:rounded-2xl"
+      />
+      <span className="flex gap-1 mt-3 bg-primary-50/5 text-primary-500 px-2 py-1 rounded-[50px] md:font-semibold">
+        <Icon>
+          <SvgCalendarDates className="w-5 h-5" />
+        </Icon>
+        Dec 20, 2021
+      </span>
+      <p className="font-nunito font-bold md:text-xl mt-4">
+        Family reunited with stolen cats after online appeal
+      </p>
+      <p className="text-sm md:text-base mt-3">
+        Lorem ipsum dolor sit amet, consec tetur adipi scing elit. Ut scing elit
+      </p>
+    </div>
+  );
+};
+
+export default TipsCard;
--- src/routes/home/components/home-tips.component.tsx
+++ src/routes/home/components/home-tips.component.tsx
@@ -1,87 +1,33 @@
 'use client';
 
-import Image from 'next/image';
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import Icon from '@/components/icon';
-
-import SvgCalendarDates from '@icons/calendar-dates.svg';
+import TipsCard from './cards/tips-card.component';
 
 const HomeTips = () => {
   return (
-    <section className="mt-16">
-      <p className="font-black font-nunito text-xl text-center">
+    <section className="mt-16 md:mt-24">
+      <p className="font-black font-nunito text-xl md:text-4xl text-center">
         tips & recommendations
       </p>
-      <Swiper className="px-5 mt-6" spaceBetween={20}>
-        <SwiperSlide className="flex flex-col items-start border border-nature-800 p-4">
-          <Image
-            src="/static/tip-cat.png"
-            alt="tip-cat"
-            width={288}
-            height={180}
-            className="w-full"
-          />
-          <span className="flex gap-1 mt-3 bg-primary-50/5 text-primary-500 px-2 py-1 rounded-[50px]">
-            <Icon>
-              <SvgCalendarDates className="w-5 h-5" />
-            </Icon>
-            Dec 20, 2021
-          </span>
-          <p className="font-nunito font-bold mt-4">
-            Family reunited with stolen cats after online appeal
-          </p>
-          <p className="text-sm mt-3">
-            Lorem ipsum dolor sit amet, consec tetur adipi scing elit. Ut scing
-            elit
-          </p>
+      <Swiper className="px-5 mt-6 md:hidden" spaceBetween={20}>
+        <SwiperSlide>
+          <TipsCard />
         </SwiperSlide>
-        <SwiperSlide className="flex flex-col items-start border border-nature-800 p-4">
-          <Image
-            src="/static/tip-cat.png"
-            alt="tip-cat"
-            width={288}
-            height={180}
-            className="w-full"
-          />
-          <span className="flex gap-1 mt-3 bg-primary-50/5 text-primary-500 px-2 py-1 rounded-[50px]">
-            <Icon>
-              <SvgCalendarDates className="w-5 h-5" />
-            </Icon>
-            Dec 20, 2021
-          </span>
-          <p className="font-nunito font-bold mt-4">
-            Family reunited with stolen cats after online appeal
-          </p>
-          <p className="text-sm mt-3">
-            Lorem ipsum dolor sit amet, consec tetur adipi scing elit. Ut scing
-            elit
-          </p>
+        <SwiperSlide>
+          <TipsCard />
         </SwiperSlide>
-        <SwiperSlide className="flex flex-col items-start border border-nature-800 p-4">
-          <Image
-            src="/static/tip-cat.png"
-            alt="tip-cat"
-            width={288}
-            height={180}
-            className="w-full"
-          />
-          <span className="flex gap-1 mt-3 bg-primary-50/5 text-primary-500 px-2 py-1 rounded-[50px]">
-            <Icon>
-              <SvgCalendarDates className="w-5 h-5" />
-            </Icon>
-            Dec 20, 2021
-          </span>
-          <p className="font-nunito font-bold mt-4">
-            Family reunited with stolen cats after online appeal
-          </p>
-          <p className="text-sm mt-3">
-            Lorem ipsum dolor sit amet, consec tetur adipi scing elit. Ut scing
-            elit
-          </p>
+        <SwiperSlide>
+          <TipsCard />
         </SwiperSlide>
       </Swiper>
+
+      <div className="hiddedn md:flex gap-6 mt-8 px-20">
+        <TipsCard />
+        <TipsCard />
+        <TipsCard />
+      </div>
     </section>
   );
 };
