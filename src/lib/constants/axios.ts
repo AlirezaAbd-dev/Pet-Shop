5b7536c275ca7a5f421c506be8af89a980a0fe68
0AliReza0 <alireza.abedi9310@gmail.com>
2024-07-05 22:28:32 +0330

    request instance change

--- src/lib/constants/axios.ts
+++ src/lib/constants/axios.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Headers':
-      'X-Requested-With,Content-Type,X-Token-Auth,Authorization',
-    Accept: 'application/json',
-  },
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Methods': '*',
+  //   'Access-Control-Allow-Credentials': 'true',
+  //   'Access-Control-Allow-Headers':
+  //     'X-Requested-With,Content-Type,X-Token-Auth,Authorization',
+  //   Accept: 'application/json',
+  // },
 });
